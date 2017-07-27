7a8bccd59b5985099ec98136b92859e06a60c822
erwinvaneyk <erwinvaneyk@gmail.com>
2017-07-27 13:37:00 -0700

    Add utility bash scripts

--- hack/codegen-grpc.sh
+++ hack/codegen-grpc.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-protoc -I . pkg/workflow-engine/workflow-engine.proto --go_out=plugins=grpc:.
+set -e
+
+protopaths=`find pkg -type f -name "*.proto"`
+while read -r path; do
+   echo "Generating golang implementations for proto-file: $path"
+   protoc -I . \
+        -I /usr/local/include \
+        -I ./pkg/ \
+        -I $GOPATH/src \
+        -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+         --proto_path=.\
+         --go_out=plugins=grpc:.\
+         ${path}
+done <<< "$protopaths"
+
+# API server (generate HTTP gateway + swagger)
+echo "Generating golang HTTP gateway and swagger definition for proto-file: pkg/apiserver/apiserver.proto"
+protoc -I . \
+        -I/usr/local/include \
+        -I ./pkg/ \
+        -I $GOPATH/src \
+        -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+        --grpc-gateway_out=logtostderr=true:. \
+        --swagger_out=logtostderr=true:. \
+        --go_out=plugins=grpc:. \
+        pkg/apiserver/apiserver.proto
---
+++ hack/format.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gofmt -w pkg/ cmd/
