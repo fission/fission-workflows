1b3fe4d9b2b7120942ed24a0b4869a3beb82e3f2
erwinvaneyk <erwinvaneyk@gmail.com>
2018-02-21 22:38:22 +0100

    Fix testify dep to avoid conflict

--- test/runtests.sh
+++ test/runtests.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+# Install test dependencies if requested
+if [ -z "$1" ]; then
+    go test -v -i $(go list ./... | grep -v '/vendor/')
+fi
+
 # Run unit and integration tests, exclude dependencies
 go test -race -v $(go list ./... | grep -v '/vendor/')
