e9781bdfc0a265cb75b6d3084a303c7fea95083a
erwinvaneyk <erwinvaneyk@gmail.com>
2018-03-16 11:51:11 +0100

    Updated swagger api

--- hack/codegen-swagger-client.sh
+++ hack/codegen-swagger-client.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
 
-mkdir cmd/wfcli/swagger-client/
-swagger generate client -f api/swagger/apiserver.swagger.json -t cmd/wfcli/swagger-client/
+mkdir -p cmd/wfcli/swagger-client/
+docker run --rm -it -e GOPATH=$HOME/go:/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger:0.11.0 generate client
+-f api/swagger/apiserver.swagger.json -t cmd/wfcli/swagger-client/
