6275f9c637d838b261df36b3c513bf9459325ad7
erwinvaneyk <erwinvaneyk@gmail.com>
2018-05-17 10:58:37 +0200

    Add whale examples to e2e tests

--- test/e2e/tests/bup_test_example_inputs.sh
+++ test/e2e/tests/bup_test_example_inputs.sh
@@ -11,4 +11,9 @@ trap cleanup EXIT
 
 fission fn create --name inputs --env workflow --src ${EXAMPLE_DIR}/inputs.wf.yaml
 sleep 5 # TODO remove this once we can initiate synchronous commands
-fission fn test --name inputs -b 'foobar' -H 'HEADER_KEY: HEADER_VAL' --method PUT | grep HEADER_KEY | grep HEADER_VAL | grep PUT | grep foobar
+fission fn test --name inputs -b 'foobar' -H 'HEADER_KEY: HEADER_VAL' --method PUT \
+    | tee /dev/tty \
+    | grep -i Header_Val \
+    | grep HEADER_VAL \
+    | grep -i PUT \
+    | grep -q foobar
