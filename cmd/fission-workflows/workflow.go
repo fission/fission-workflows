61b6703f66e3945144bf3ccee06e5185a3da02cd
erwinvaneyk <erwinvaneyk@gmail.com>
2019-01-05 16:33:38 +0100

    cli: added workflow create command

--- cmd/fission-workflows/workflow.go
+++ cmd/fission-workflows/workflow.go
@@ -5,7 +5,9 @@ import (
 	"os"
 	"sort"
 
+	"github.com/fission/fission-workflows/pkg/parse"
 	"github.com/fission/fission-workflows/pkg/parse/yaml"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
@@ -14,6 +16,46 @@ var cmdWorkflow = cli.Command{
 	Aliases: []string{"wf", "workflows"},
 	Usage:   "Workflow-related commands",
 	Subcommands: []cli.Command{
+		{
+			Name:  "create",
+			Usage: "Define a workflow within the workflow engine.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "src",
+					Usage: "Path to the YAML or Protobuf workflow definition file",
+				},
+				cli.StringFlag{
+					Name:  "name",
+					Usage: "Name of the workflow",
+				},
+			},
+			Action: commandContext(func(ctx Context) error {
+				client := getClient(ctx)
+
+				// Fetch and parse the workflow
+				srcPath := ctx.String("src")
+				if len(srcPath) == 0 {
+					logrus.Fatalf("Requires workflow definition file. Use `--src <file>`.")
+				}
+				fd, err := os.Open(srcPath)
+				if err != nil {
+					logrus.Fatalf("Failed to open workflow definition file: %v", err)
+				}
+				spec, err := parse.Parse(fd)
+				if err != nil {
+					logrus.Fatal(err)
+				}
+				spec.Name = ctx.String("name")
+
+				// Create workflow
+				md, err := client.Workflow.Create(ctx, spec)
+				if err != nil {
+					logrus.Fatalf("Failed to create workflow: %v", err)
+				}
+				fmt.Println(md.Id)
+				return nil
+			}),
+		},
 		{
 			Name:  "get",
 			Usage: "get <Workflow-id> <task-id>",
@@ -41,7 +83,7 @@ var cmdWorkflow = cli.Command{
 						rows = append(rows, []string{wfID, wf.Spec.Name, string(wf.Status.Status),
 							created, updated})
 					}
-					table(os.Stdout, []string{"id", "NAME", "STATUS", "CREATED", "UPDATED"}, rows)
+					table(os.Stdout, []string{"ID", "NAME", "STATUS", "CREATED", "UPDATED"}, rows)
 				case 1:
 					// Get Workflow
 					wfID := ctx.Args().Get(0)
